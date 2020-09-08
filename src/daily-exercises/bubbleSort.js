fd0f483ddde91da25675572f3fbf47460c14dea3
dmitridrozdov <dm.drozdov@gmail.com>
2020-09-09 09:13:48 +1000

    added countSwaps

---
+++ src/daily-exercises/bubbleSort.js
@@ -0,0 +1,22 @@
+function countSwaps(a) {
+  let swaps = 0;
+  for (let i = 0; i < a.length; i++) {
+    for (let j = 0; j < a.length - 1; j++) {
+      // Swap adjacent elements if they are in decreasing order
+      if (a[j] > a[j + 1]) {
+        // swap(a[j], a[j + 1]);
+        let tmp = a[j];
+        a[j] = a[j + 1];
+        a[j + 1] = tmp;
+        swaps++;
+      }
+    }
+  }
+  console.log("Array is sorted in " + swaps + " swaps.");
+  console.log("First Element: " + a[0]);
+  console.log("Last Element: " + a[a.length - 1]);
+}
+
+let a = [1, 2, 3];
+
+let result = countSwaps(a);
