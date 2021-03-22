99cf9402e12009863122cf7b6ccfb7e283d549d7
dmitridrozdov <dm.drozdov@gmail.com>
2021-03-22 15:19:37 +1100

    Create AppComponent.js

---
+++ src/traversy-crash-course/AppComponent.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from './components/Header'
+
+const AppComponent = () => {
+    return (
+        <div>
+            <h1>Traversy Crash Course</h1>
+            <Header title='new title' />
+        </div>
+    )
+}
+
+export default AppComponent
