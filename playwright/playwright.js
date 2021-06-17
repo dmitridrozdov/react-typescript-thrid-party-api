6c30c063e1cf9fff6ec0728eccd9fdc05431a35e
dmitridrozdov <dm.drozdov@gmail.com>
2021-06-17 17:24:01 +1000

    Update playwright.js

--- playwright/playwright.js
+++ playwright/playwright.js
@@ -1,9 +1,19 @@
-const { chromium } = require('playwright');
+const { firefox } = require('playwright');
+
+// (async () => {
+//   const browser = await chromium.launch({ headless: false, slowMo: 100 });
+//   const page = await browser.newPage();
+//   await page.goto('http://whatsmyuseragent.org/');
+// //   await page.screenshot({ path: `example.png` });
+//   await browser.close();
+// })();
 
 (async () => {
-  const browser = await chromium.launch({ headless: false, slowMo: 100 });
-  const page = await browser.newPage();
-  await page.goto('http://whatsmyuseragent.org/');
-  await page.screenshot({ path: `example.png` });
-  await browser.close();
-})();
+    const browser = await firefox.launch({ headless: false, slowMo: 100 })
+    // const context = await browser.newContext({ viewport: null })
+    const page = await browser.newPage()
+    await page.goto('http://localhost:3000/weather')
+    await page.fill('.search-bar', 'Lviv')
+    await page.keyboard.press('Enter')
+    // await browser.close();
+  })();
