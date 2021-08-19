eff982c3b3207833a69d038bc61c97ac61b1d990
dmitridrozdov <dm.drozdov@gmail.com>
2021-08-19 13:00:56 +1000

    Update hackerRankString.js

--- src/daily-exercises/hackerrank/hackerRankString.js
+++ src/daily-exercises/hackerrank/hackerRankString.js
@@ -1,34 +1,35 @@
 const word = 'hackerrank'
 
-// const findCharReturnSlice = (ch, str) => {
-//     const index = str.indexOf(ch)
-//     return index//str.slice(index)
-// }
 
 const recursiveCheck = (word, str) => {
     if(word.length === 0) {
         return 'YES'
     }
     const index = str.indexOf(word[0])
+    console.log('something wrong what is the index: ' + index)
     if(index === -1) {
+        console.log('index equal -1')
         return 'NO'
     } else {
         console.log('----')
-        console.log(word.slice(1))
-        console.log(str.slice(index))
-        recursiveCheck(word.slice(1), str.slice(index))
+        console.log(word)
+        console.log(str)
+        console.log('current letter to check: ' + word[0]) 
+        console.log('current index: ' + index)
+        recursiveCheck(word.slice(1), str.slice(index + 1))
     } 
 } 
 
 const hackerrankInString = (s) => {
-    let currentWord = word
-    return s.indexOf(word[1])
+    return recursiveCheck(word, s)
 }
 
 // const s = 'hhaacckkekraraannk'
-// const s = 'ddddddd'
-// const result = hackerrankInString(s)
-// console.log(result)
+// const s = 'haddddddd'
+const s = 'llllhdddaiiiii'
+// const s = 'ddddd'
+const result = hackerrankInString(s)
+console.log(result)
 
 // const s = 'hhaacckkekraraannk'
 // const resultSlice = recursiveCheck('a', s)
