3e8e9b5c3e4901b52225827060812b156bc66577
dmitridrozdov <dm.drozdov@gmail.com>
2021-08-20 12:01:21 +1000

    Create superReducedString.js

---
+++ src/daily-exercises/hackerrank/superReducedString.js
@@ -0,0 +1,15 @@
+const superReducedString = (s) => {
+ let output = s.split('')
+ for(let i = 0; i < output.length; i++) {
+   if(output[i] === output[i+1]){
+     output.splice(i, 2)
+     i = -1
+    }
+  }
+ return output.length === 0 ? 'Empty String' : output.join('')
+}
+
+// const s = 'aaabccddd'
+const s = 'abba'
+const result = superReducedString(s)
+console.log(result)
