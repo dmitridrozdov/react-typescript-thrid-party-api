5e306fc069dbb8a14dee66f0d25d6337c6fdd805
dmitridrozdov <dm.drozdov@gmail.com>
2021-08-20 10:09:02 +1000

    Update hackerRankString.js

--- src/daily-exercises/hackerrank/hackerRankString.js
+++ src/daily-exercises/hackerrank/hackerRankString.js
@@ -1,36 +1,18 @@
-const word = 'hackerrank'
-
-
 const recursiveCheck = (word, str) => {
-    if(word.length === 0) {
-        return 'YES'
-    }
+    if(word.length === 0) { return 'YES' }
     const index = str.indexOf(word[0])
-    console.log('something wrong what is the index: ' + index)
-    if(index === -1) {
-        console.log('index equal -1')
-        return 'NO'
-    } else {
-        console.log('----')
-        console.log(word)
-        console.log(str)
-        console.log('current letter to check: ' + word[0]) 
-        console.log('current index: ' + index)
-        recursiveCheck(word.slice(1), str.slice(index + 1))
-    } 
+    if(index === -1) { return 'NO' } 
+    else { return recursiveCheck(word.slice(1), str.slice(index + 1)) } 
 } 
 
 const hackerrankInString = (s) => {
+    const word = 'hackerrank'
     return recursiveCheck(word, s)
 }
 
 // const s = 'hhaacckkekraraannk'
-// const s = 'haddddddd'
-const s = 'llllhdddaiiiii'
+const s = 'haddddddd'
+// const s = 'llllhdddaiiiii'
 // const s = 'ddddd'
 const result = hackerrankInString(s)
 console.log(result)
-
-// const s = 'hhaacckkekraraannk'
-// const resultSlice = recursiveCheck('a', s)
-// console.log(resultSlice)
