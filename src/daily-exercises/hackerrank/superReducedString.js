2c2e2df8d66d68d76a1c929265eae49cb823f3e9
dmitridrozdov <dm.drozdov@gmail.com>
2021-08-23 09:31:00 +1000

    Update superReducedString.js

--- src/daily-exercises/hackerrank/superReducedString.js
+++ src/daily-exercises/hackerrank/superReducedString.js
@@ -9,7 +9,22 @@ const superReducedString = (s) => {
  return output.length === 0 ? 'Empty String' : output.join('')
 }
 
+const reducedRecursive = (s, index) => {
+    if(s.length === index) return s
+    if(s[index] === s[index+1]) {
+        s.splice(index, 2)
+        return reducedRecursive(s, 0)
+    }
+    return reducedRecursive(s, index+1)
+}
+
+const superReducedString2 = (s) => {
+    let charsArray = s.split('')
+    let result = reducedRecursive(charsArray, 0)
+    return result.length === 0 ? 'Empty String' : result.join('')
+}
+
 // const s = 'aaabccddd'
 const s = 'abba'
-const result = superReducedString(s)
+const result = superReducedString2(s)
 console.log(result)
