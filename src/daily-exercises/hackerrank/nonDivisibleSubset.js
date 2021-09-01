e8494b49d4471436fcaa3fe89c0d87d107b45fea
dmitridrozdov <dm.drozdov@gmail.com>
2021-09-01 13:32:29 +1000

    Update nonDivisibleSubset.js

--- src/daily-exercises/hackerrank/nonDivisibleSubset.js
+++ src/daily-exercises/hackerrank/nonDivisibleSubset.js
@@ -24,11 +24,19 @@ const recursiveShift = (k, s, result) => {
 
 const nonDivisibleSubset = (k, s) => {
     const result = recursiveShift(k, s, [])
-    return [...new Set([].concat(...result))] //flatten and remove duplications
+    console.log(result)
+    const flattenRemoveDupl = [...new Set([].concat(...result))] //flatten and remove duplications
+    return flattenRemoveDupl.length
 }
 
-const k = 3
-const s = [1, 7, 2, 4]
+// const k = 3
+// const s = [1, 7, 2, 4]
+
+// const k = 4
+// const s = [19, 10, 12, 10, 24, 25, 22]
+
+const k = 7
+const s = [278, 576, 496, 727, 410, 124, 338, 14, 209, 702, 282, 718, 771, 575, 436]
 
 console.log(nonDivisibleSubset(k, s))
 
