ef5367261c55aa5fb65cf513c7f496a12103c7dd
dmitridrozdov <dm.drozdov@gmail.com>
2021-09-01 15:39:56 +1000

    Update nonDivisibleSubset.js

--- src/daily-exercises/hackerrank/nonDivisibleSubset.js
+++ src/daily-exercises/hackerrank/nonDivisibleSubset.js
@@ -29,16 +29,41 @@ const nonDivisibleSubset = (k, s) => {
     return flattenRemoveDupl.length
 }
 
-// const k = 3
-// const s = [1, 7, 2, 4]
+function nonDivisibleSubset1(k, s) {
+    // Write your code here
+    let values = new Array(k).fill(0);
+    let result = 0;
+  
+    s.reduce((target, item, index) => {
+      values[item % k] += 1;
+  
+      return target;
+    }, []);
+
+    for (let i of Array.from(
+      { length: (k + 1) / 2 - 1 },
+      (value, index) => index + 1
+    )) {
+      result += Math.max(values[i], values[k - i]);
+    }
+  
+    !(k % 2) && !!values[k / 2] && (result += 1);
+  
+    values[0] && (result += 1);
+  
+    return result;
+  }
+
+const k = 3
+const s = [1, 7, 2, 4]
 
 // const k = 4
 // const s = [19, 10, 12, 10, 24, 25, 22]
 
-const k = 7
-const s = [278, 576, 496, 727, 410, 124, 338, 14, 209, 702, 282, 718, 771, 575, 436]
+// const k = 7
+// const s = [278, 576, 496, 727, 410, 124, 338, 14, 209, 702, 282, 718, 771, 575, 436]
 
-console.log(nonDivisibleSubset(k, s))
+console.log(nonDivisibleSubset1(k, s))
 
 // let result = []
 
