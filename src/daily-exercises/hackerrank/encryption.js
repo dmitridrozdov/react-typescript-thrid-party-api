1291ff276436a01b391b24c7282a852850ada179
dmitridrozdov <dm.drozdov@gmail.com>
2021-09-13 11:42:29 +1000

    Update encryption.js

--- src/daily-exercises/hackerrank/encryption.js
+++ src/daily-exercises/hackerrank/encryption.js
@@ -1,7 +1,20 @@
 const encryption = (s) => {
     const nChars = Math.ceil(Math.sqrt(s.length))
-    return s.match(new RegExp('.{1,' + nChars + '}', 'g'))
+    const rowsStr = s.match(new RegExp('.{1,' + nChars + '}', 'g'))
+    let result = ''
+    const firstRowLenght = rowsStr[0].length
+    for(let i = 0; i < firstRowLenght; i++) {
+        for(let j = 0; j < rowsStr.length; j++) {
+            console.log(rowsStr[j] + ': ' + rowsStr[j].length + '  current i: ' + i)
+            if(rowsStr[j].length > i) {
+                result += rowsStr[j][i]
+            }
+        }
+        result += ' '
+    }
+    return result
 }
 
-const s = 'haveaniceday'
+// const s = 'haveaniceday'
+const s = 'feedthedog'
 console.log(encryption(s))
