cb6e6a8385eb17b00b62fe02009f1d93c6f80d2a
dmitridrozdov <dm.drozdov@gmail.com>
2021-09-21 09:49:52 +1000

    Update fillingJars.js

--- src/daily-exercises/hackerrank/fillingJars.js
+++ src/daily-exercises/hackerrank/fillingJars.js
@@ -1,6 +1,14 @@
+const fillJarsByRange = (arr, operation) => {
+    const startIndex = operation[0]
+    const endIndex = operation[0]
+    const value = operation[2]
+    const result = arr.map((a, index) => 
+    return value
+}
+
 const fillingJars = (n, operations) => {
     let a = [...Array(n)].fill(0)
-    return a
+    return fillJarsByRange(a, operations[0])
 }
 
 const n = 5
