f86f12a291bfac4714bc0191deecd78f709f0df6
dmitridrozdov <dm.drozdov@gmail.com>
2021-09-22 11:55:46 +1000

    Update fillingJars.js

--- src/daily-exercises/hackerrank/fillingJars.js
+++ src/daily-exercises/hackerrank/fillingJars.js
@@ -22,23 +22,6 @@ const fillingJars = (n, operations) => {
     return Math.floor(resultArray.reduce((a, b) => a + b, 0) / n)
 }
 
-function processData(input) {
-    var lines = input.split("\n");
-    var head = lines[0].split(" ").map(i => parseInt(i));
-    var jars = head[0];
-    var operations = head[1];
-    var sum = 0;
-    for(var i = 1; i < lines.length; i++) {
-        var nums = lines[i].split(" ").map(i => parseInt(i));
-        var a = nums[0];
-        var b = nums[1];
-        var candies = nums[2];
-        sum += (b - a + 1) * candies;
-    }
-    var avg = sum / jars;
-    console.log(parseInt(Math.floor(avg)));
-}
-
 const fillingJars2 = (n, operations) => {
     let sum = 0
     for(let i = 0; i < operations.length; i++) {
