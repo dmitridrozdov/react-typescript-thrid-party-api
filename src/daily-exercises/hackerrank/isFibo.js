b0d32e657d9b003eaf166e02582ffb4809b6ecac
dmitridrozdov <dm.drozdov@gmail.com>
2021-10-11 12:07:36 +1100

    Update isFibo.js

--- src/daily-exercises/hackerrank/isFibo.js
+++ src/daily-exercises/hackerrank/isFibo.js
@@ -1,6 +1,20 @@
-const isFibo = (n) => {
-
+let isFiboPavol = n => {
+    let fib0 = 0;
+    let fib1 = 1;
+    while(fib0 <= n) {
+        if (fib0 == n) return "IsFibo";
+        let temp = fib0;
+        fib0 = fib1;
+        fib1 += temp;
+    }
+    return "IsNotFibo"
 }
 
-const n = 5
+const isFibo = (n) => {
+    const result = Math.sqrt(5 * n) - 4
+    return result
+}
+
+const n = 11
 console.log(isFibo(n))
+console.log(isFiboPavol(n))
