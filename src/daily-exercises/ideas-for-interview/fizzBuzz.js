ab00673bdf36028a78a3c43a43ddf75373090245
dmitridrozdov <dm.drozdov@gmail.com>
2021-12-21 12:27:52 +1100

    Update fizzBuzz.js

--- src/daily-exercises/ideas-for-interview/fizzBuzz.js
+++ src/daily-exercises/ideas-for-interview/fizzBuzz.js
@@ -1,3 +1,10 @@
+// Write a program that prints the numbers from 1 to 100.
+// But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”.
+// For numbers which are multiples of both three and five print “FizzBuzz”.
+
+// 1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, FizzBuzz...
+
+
 const fizzBuzz = (num) => {
     for(let i=1; i<=num; i++) {
         if(i%3 === 0 && i%5 === 0) {
