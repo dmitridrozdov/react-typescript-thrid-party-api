488c4e6679d96ae72dd4ed4417e4fa01a90d9874
dmitridrozdov <dm.drozdov@gmail.com>
2022-01-18 15:06:21 +1100

    Update spreadJson.js

--- src/daily-exercises/spreadJson.js
+++ src/daily-exercises/spreadJson.js
@@ -4,3 +4,7 @@ const updatedJson = { ...jsonTest, age: '31', surename: 'Voron'}
 
 console.log(jsonTest)
 console.log(updatedJson)
+
+//verify slice
+const str = 'AlexUSDT'
+console.log(str.slice(-4))
