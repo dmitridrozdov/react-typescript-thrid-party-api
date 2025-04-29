e8a1cb2fda253be58d44cee32b621db995013b5d
dmitridrozdov <dm.drozdov@gmail.com>
2025-04-29 10:57:34 +1000

    Update investmentMaximumIncome.js

--- src/daily-exercises/ideas-for-interview/investmentMaximumIncome.js
+++ src/daily-exercises/ideas-for-interview/investmentMaximumIncome.js
@@ -1,22 +1,72 @@
-const solution = (investment, income) => {
-    let maxIncome = 0;
-    let maxInvestment = 0;
+// const solution = (investment) => {
+//     let maxIncome = investment[0]
+//     let assetStatus = true
+//     let bestPrice = 0
+//     for (let i = 0; i < investment.length - 1; i++) {
+//         if(assetStatus) {
+//             if (investment[i] > investment[i + 1]) {
+//                 maxIncome += investment[i]
+//                 bestPrice = 0
+//                 assetStatus = false
+//         }
+//         } else {
+//             if (investment[i] < investment[i + 1]) {
+//                 maxIncome -= investment[i]
+//                 assetStatus = true
+//             }
+//         }
+//     }
     
-    for (let i = 0; i < investment.length; i++) {
-        if (investment[i] > maxInvestment) {
-        maxInvestment = investment[i];
-        maxIncome = income[i];
-        } else if (investment[i] === maxInvestment) {
-        maxIncome = Math.max(maxIncome, income[i]);
+//     return maxIncome;
+// }; 
+
+
+// function solution(A) {
+//     const MOD = 1000000000;
+//     let income = A[0]; // initially sell
+//     for (let i = 1; i < A.length; i++) {
+//         if (A[i] > A[i-1]) {
+//             income += (A[i] - A[i-1]);
+//         }
+//     }
+//     return income % MOD;
+// }
+
+
+function solution(prices) {
+    let maxProfit = 0;
+    let minPrice = Infinity;
+
+    for (let i = 0; i < prices.length; i++) {
+        if (prices[i] < minPrice) {
+            minPrice = prices[i];
+        } else if (prices[i] > minPrice) {
+            maxProfit += prices[i] - minPrice;
+            minPrice = prices[i]; // Reset minPrice for the next potential transaction
         }
     }
-    
-    return maxIncome;
+
+    return maxProfit;
 }
 
-// Example usage:
-const investment = [100, 200, 300, 400, 500];
-const income = [10, 20, 30, 40, 50];
-console.log(solution(investment, income)); // Output: 50
-// Explanation:
-// The maximum investment is 500, and the corresponding income is 50.
+// Examples:
+const A1 = [4, 1, 2, 3];
+console.log(solution(A1)); // Output: 6
+
+const A2 = [1, 2, 3, 3, 2, 1, 5];
+console.log(solution(A2)); // Output: 7
+
+const A3 = [1000000000, 1, 2, 2, 1000000000, 1, 1000000000];
+console.log(solution(A3)); // Output: 999999998
+
+// const A4 = [1, 5];
+// console.log(solution(A4)); // Output: 4
+
+// const A5 = [5, 1];
+// console.log(solution(A5)); // Output: 0
+
+// const A6 = [1, 2, 3, 4, 5];
+// console.log(solution(A6)); // Output: 4
+
+// const A7 = [5, 4, 3, 2, 1];
+// console.log(solution(A7)); // Output: 0
